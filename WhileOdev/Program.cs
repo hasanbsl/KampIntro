8d1c165ff5da0f63df6d205b222bd7dccdd36c05
hasan başalan <hasanbasalan05@gmail.com>
2021-01-21 23:44:20 +0300

    Add project files.

---
+++ WhileOdev/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WhileOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            while (n <= 41)
+            {
+                Console.WriteLine("Maşallah!");
+                n++;
+            }
+<<<<<<< HEAD
+
+            Console.WriteLine("Döngü bitişi");
+=======
+>>>>>>> f58031b1469cae035685ac7da982cc6a18c6367f
+        }
+    }
+}
